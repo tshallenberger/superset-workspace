74eaa9f200f4415208bd016d186c61de233f1c63
tshallenberger <twshal@gmail.com>
2024-04-30 04:45:46 +0000

    ext: updated superset_config

---
+++ extensions/custom_sso_security_manager.py
@@ -0,0 +1,22 @@
+import logging
+from superset.security import SupersetSecurityManager
+
+
+class CustomSsoSecurityManager(SupersetSecurityManager):
+    def oauth_user_info(self, provider, response=None):
+        logging.debug("[AUTH] oauth2 provider: {0}.".format(provider))
+        if provider == "okta":
+            me = self.appbuilder.sm.oauth_remotes[provider].userinfo()
+            logging.debug("[AUTH] userinfo: {0}".format(me))
+
+            if me is None:
+                return None
+            else:
+                return {
+                    "name": me["name"],
+                    "email": me["primary_email"],
+                    "id": me["short_id"],
+                    "username": me["short_id"],
+                    "first_name": me["first_name"],
+                    "last_name": me["last_name"],
+                }
