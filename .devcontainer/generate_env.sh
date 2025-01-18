ebc33e58592b53c3e56fbc6f65abdaaf279dcab6
tshallenberger <twshal@gmail.com>
2025-01-18 23:06:00 +0000

    dev: working 4.1.1 devcontainer

--- .devcontainer/generate_env.sh
+++ .devcontainer/generate_env.sh
@@ -4,13 +4,14 @@ USER_UID=$(id -u)
 USER_GID=$(id -g)
 
 # Write the environment variables to the .env file
-echo "USER_UID=$USER_UID" > .devcontainer/.env
-echo "USER_GID=$USER_GID" >> .devcontainer/.env
+echo "UID=$USER_UID" > .devcontainer/.env
+echo "GID=$USER_GID" >> .devcontainer/.env
 echo "USER=$USER" >> .devcontainer/.env
 echo "TLS=$HOME/.tls" >> .devcontainer/.env
 echo "CERTS=$HOME/.certs" >> .devcontainer/.env
+echo "EXPORTS=.devcontainer/exports" >> .devcontainer/.env
 echo "MYSQL_DATA=./volumes/mysql/data" >> .devcontainer/.env
-echo "MYSQL_INI=./volumes/mysql/ini/0_init.sql" >> .devcontainer/.env
+echo "MYSQL_INI=./volumes/mysql/ini" >> .devcontainer/.env
 echo "MYSQL_ROOT_PASSWORD=superset" >> .devcontainer/.env
 echo "MYSQL_USER=superset" >> .devcontainer/.env
 echo "MYSQL_PASSWORD=superset" >> .devcontainer/.env
